f09ee27775b6fed533aa7bd47628ad77f413a788
dan <https://github.com/DanilloSB/Projeto-Cordova2.git>
2021-06-20 21:40:29 -0300

    corrigi erro de syntax

--- web/js/main.js
+++ web/js/main.js
@@ -1,7 +1,7 @@
 if ('serviceWorker' in navigator) {
 	console.log("main.js => Vamos registrar o service worker!!!");
 	navigator.serviceWorker.register('/service-worker-minimum-to-intall-pwa.js')
-		.then(registration) => {
+		.then(registration => {
 			console.log("main.js => Service Worker Registrado com Sucesso");
 			console.dir(registration);
 	})
